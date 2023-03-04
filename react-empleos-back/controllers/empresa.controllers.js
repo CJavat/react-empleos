b5c5ac302671b6b92df8f983f427fe9cefa33336
Dani Plasc <72353993+CJavat@users.noreply.github.com>
2023-03-04 11:34:54 -0600

    CONTROLADOR EMPRESA ACTUALIZADO

--- react-empleos-back/controllers/empresa.controllers.js
+++ react-empleos-back/controllers/empresa.controllers.js
@@ -1,16 +1,68 @@
 const Empresa = require("../models/Empresa.models");
 
-const registrarEmpresa = (req, res, next) => {
+//! REGISTRAR UNA EMPRESA --
+const registrarEmpresa = async (req, res, next) => {
+  const { idUsuario } = req.params;
+
+  const esReclutador = await Empresa.findOne({
+    reclutador: idUsuario,
+  }).populate("reclutador");
+
+  if (esReclutador.reclutador.rol === "Empleado") {
+    return res.json({
+      msg: "Los empledos no pueden registrar empresa, registrate como un Reclutador",
+    });
+  }
+
   const empresa = new Empresa(req.body);
 
-  try {
-    empresa.save();
-    res.json({ msg: "Empresa Registrada Correctamente" });
-  } catch (error) {
-    return res.json({ msg: "Ocurrió un error al agregar la empresa" });
-  }
+  if (empresa.reclutador)
+    try {
+      empresa.save();
+      res.json({ msg: "Empresa Registrada Correctamente" });
+    } catch (error) {
+      return res.json({ msg: "Ocurrió un error al agregar la empresa" });
+    }
 };
 
+//! MOSTRAR UNA EMPRESA POR SU ID --
+const mostrarEmpresa = async (req, res, next) => {
+  //* Obtener el id del usuario --
+  const { idEmpresa } = req.params;
+  const empresaEncontrada = await Empresa.findOne({
+    reclutador: idEmpresa,
+  }).populate("reclutador");
+
+  if (!empresaEncontrada) {
+    return res.json({ msg: "No se ha encontrado la empresa" });
+  }
+
+  res.json(empresaEncontrada);
+
+  console.log(empresaEncontrada);
+};
+
+//! MOSTRAR TODAS LAS EMPRESAS --
+const mostrarEmpresas = async (req, res, next) => {
+  const empresas = await Empresa.find().select("-__v");
+  if (!empresas) {
+    return res.json({ msg: "No hay empresas registradas" });
+  }
+
+  res.json(empresas);
+};
+
+//! ACTUALIZAR DATOS DE LA EMPRESA MEDIANTE UN ID --
+const actualizarEmpresa = async (req, res, next) => {};
+
+//! ELIMINAR EMPRESA MEDIANTE UN ID --
+const eliminarEmpresa = async (req, res, next) => {};
+
+//! EXPORTAR CONTROLADORES --
 module.exports = {
   registrarEmpresa,
+  mostrarEmpresa,
+  mostrarEmpresas,
+  actualizarEmpresa,
+  eliminarEmpresa,
 };
--- react-empleos-back/routes/empresa.routes.js
+++ react-empleos-back/routes/empresa.routes.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-const { registrarEmpresa } = require("../controllers/empresa.controllers");
+const {
+  registrarEmpresa,
+  mostrarEmpresa,
+  mostrarEmpresas,
+  actualizarEmpresa,
+  eliminarEmpresa,
+} = require("../controllers/empresa.controllers");
 
 //! REGISTRAR LOS DATOS DE LA EMPRESA --
 router.post("/registrar-empresa/:idUsuario", registrarEmpresa);
 
 //! MOSTRAR EMPRESA CON LOS DATOS DEL RECLUTADOR --
+router.get("/mostrar-empresa/:idEmpresa", mostrarEmpresa);
 
 //! MOSTRAR EMPRESAS --
+router.get("/mostrar-empresas", mostrarEmpresas);
 
 //! ACTUALIZAR DATOS DE UNA EMPRESA --
+router.put("/actualizar-empresa", actualizarEmpresa);
 
 //! ELIMINAR EMPRESA --
+router.delete("/eliminar-empresa", eliminarEmpresa);
 
 module.exports = router;
